343d3b226cfead5d435a162f98a98b052641aa49
Linyugui <446046940@qq.com>
2018-04-28 10:38:52 +0800

    修改了2.php的flag以及4.js集合遍历

--- 2-primeIn100.php
+++ 2-primeIn100.php
@@ -2,15 +2,15 @@
 
 for ($i = 2; $i <= 100 ; $i++) { 
 	# code...
-	$falg = true;
+	$flag = true;
 	for ($j = 2; $j <= $i/2 ; $j++) { 
 		# code...
 		if ($i % $j == 0) {
 			# code...
-			$falg = false;
+			$flag = false;
 		}
 	}
-	if($falg)
+	if($flag)
 	{
 		echo $i.'&nbsp;';
 	}
--- 4-car-selection.js
+++ 4-car-selection.js
@@ -1,7 +1,7 @@
 
 
-var car=[['benz-S800',80],['bmw-X7',90],['maybach',120],['lada',10]];
+var car={'benz-S800':80,'bmw-X7':90,'maybach':120,'lada':10};
 for (var i in car) {
-	if(car[i][1]<95)
-		console.log(car[i][0]);
+	if(car[i]<95)
+		console.log(i);
 }
